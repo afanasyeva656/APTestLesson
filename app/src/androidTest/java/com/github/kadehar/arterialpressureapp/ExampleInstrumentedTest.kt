cbe31c9e6a3d9fe8cd2f2a7a0009488e80836152
asya afanasyeva <afanasyeva656@gmail.com>
2021-12-07 16:49:10 +0200

    Added setTextIntoAPDetailsMorningText and isFiltersVisibleOnAPListFragment tests

--- app/src/androidTest/java/com/github/kadehar/arterialpressureapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/github/kadehar/arterialpressureapp/ExampleInstrumentedTest.kt
@@ -2,9 +2,9 @@ package com.github.kadehar.arterialpressureapp
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -34,8 +34,25 @@ class ExampleInstrumentedTest {
     }
 
     @Test
-    fun changesOnClick() {
+    fun changeIconButtonOnApDetailCreateButtonClick() {
         onView(withId(R.id.apDetailsCreateButton)).perform(click())
         onView(withId(R.id.apDetailsSaveButton)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun setTextIntoAPDetailsMorningText() {
+        val apRecordToBeTyped = "12080"
+        val apRecordToBeSeen = "120/80"
+
+        onView(withId(R.id.apDetailsCreateButton)).perform(click())
+        onView(withId(R.id.apDetailsMorningText)).perform(typeText(apRecordToBeTyped))
+
+        onView(withId(R.id.apDetailsMorningText))
+            .check(matches(withText(apRecordToBeSeen)))
+    }
+
+    @Test
+    fun isFiltersVisibleOnAPListFragment() {
+        onView(withId(R.id.arterialPressureFilters)).check(matches(isDisplayed()))
     }
 }
